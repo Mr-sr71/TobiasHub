4c3e248e8ffb24ed800521e22b67b7d29a1a4113
Mr-sr71 <jasperserkoch@gmail.com>
2026-08-22 10:27:40 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,9 @@
 -- ==========================================
 -- Official Script Created by @Tobiastheeighth
 -- All Rights Reserved. Stealing/Rebranding is prohibited.
+-- You can find Tobias Hub on ScriptBlox and Pastefy
 -- ==========================================
 
 print("Loading Official Tobias Hub by @Tobiastheeighth...")
-loadstring(game:HttpGet("https://githubusercontent.com"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mr-sr71/TobiasHub/refs/heads/main/main.lua"))()
+
