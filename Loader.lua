41354d529da121714765994e83536db9303ab34b
Mr-sr71 <jasperserkoch@gmail.com>
2026-08-22 10:25:11 -0400

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+-- ==========================================
+-- Official Script Created by @Tobiastheeighth
+-- All Rights Reserved. Stealing/Rebranding is prohibited.
+-- ==========================================
+
+print("Loading Official Tobias Hub by @Tobiastheeighth...")
+loadstring(game:HttpGet("https://githubusercontent.com"))()
