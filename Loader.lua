b09f90b4c7c63e310118bb4396991fdbace7880d
Mr-sr71 <jasperserkoch@gmail.com>
2026-08-23 17:13:10 -0400

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,8 +0,0 @@
--- ==========================================
--- Official Script Created by @Tobiastheeighth
--- All Rights Reserved. Stealing/Rebranding is prohibited.
--- You can find Tobias Hub on ScriptBlox, Pastefy, and Github
--- ==========================================
-
-print("Loading Official Tobias Hub by @Tobiastheeighth...")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mr-sr71/TobiasHub/refs/heads/main/main.lua"))()
