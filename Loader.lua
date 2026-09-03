c7737a0c456c40cd3576403e5d905e2e69c2f268
Mr-sr71 <jasperserkoch@gmail.com>
2026-09-03 19:28:53 -0400

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+-- ==========================================
+-- Official Script Created by @Tobiastheeighth
+-- All Rights Reserved. Stealing/Rebranding is prohibited.
+-- You can find Tobias Hub on ScriptBlox, Rscripts, Pastefy, and Github
+-- Rscripts and Scriptblox Username is Tobias_Scripts
+-- Youtube channels are: @Tobiastheeighth and @DonnellandJasperRBLX
+-- ==========================================
+
